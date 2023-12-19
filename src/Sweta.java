1b52a5b5c4c93b47fe7d51fad768a670258a5437
dell <dell@DESKTOP-E57KD5O>
2023-12-19 14:27:42 +0530

    commit

---
+++ src/Sweta.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Sweta {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		if(n%2==0)
+			System.out.println(n+ " it is even no");
+			
+		else
+			System.out.println(n+ " it is odd no");
+		
+
+	}
+
+}
